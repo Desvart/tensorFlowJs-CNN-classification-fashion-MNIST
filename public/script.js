4b4f1f9b75b737142ac06b7a20046243d29e0556
dek <derek.svart@gmail.com>
2024-08-07 16:44:03 +0200

    Multi-layer perceptron for non linear regression estimation

--- public/script.js
+++ public/script.js
@@ -1,10 +1,16 @@
-import {TRAINING_DATA} from 'https://storage.googleapis.com/jmstore/TensorFlowJS/EdX/TrainingData/real-estate-data.js';
+const INPUTS = [];
+for (let n = 1; n <=20; n++) {
+    INPUTS.push(n);
+}
 
-const INPUTS = TRAINING_DATA.inputs;
-const OUTPUTS = TRAINING_DATA.outputs;
+const OUTPUTS = [];
+for (let n = 0; n < INPUTS.length; n++) {
+    OUTPUTS.push(INPUTS[n] * INPUTS[n]);
+}
+
 tf.util.shuffleCombo(INPUTS, OUTPUTS);
 
-const INPUTS_TENSOR = tf.tensor2d(INPUTS);
+const INPUTS_TENSOR = tf.tensor1d(INPUTS);
 const OUTPUTS_TENSOR = tf.tensor1d(OUTPUTS);
 
 function normalize(tensor, min, max) {
@@ -34,34 +40,47 @@ FEATURE_RESULTS.MAX_VALUES.print();
 INPUTS_TENSOR.dispose();
 
 const model = tf. sequential();
-model.add(tf.layers.dense({inputShape: [2], units: 1}));
+model.add(tf.layers.dense({inputShape: [1], units: 25, activation: 'relu'}));
+model.add(tf.layers.dense({units: 5, activation: 'relu'}));
+model.add(tf.layers.dense({units: 1}));
 
 model.summary();
 
+const LEARNING_RATE = 0.0001
+const OPTIMIZER = tf.train.sgd(LEARNING_RATE);
+
 async function train() {
-    const LEARNING_RATE = 0.01;
+
 
     model.compile({
-       optimizer: tf.train.sgd(LEARNING_RATE),
+       optimizer: OPTIMIZER,
        loss: 'meanSquaredError'
     });
 
+    function logProgress(epoch, logs) {
+        console.log(`Epoch: ${epoch} - loss: ${Math.sqrt(logs.loss)}`);
+        if (epoch === 70) {
+            OPTIMIZER.setLearningRate(LEARNING_RATE / 2);
+        }
+    }
+
     let results = await model.fit(FEATURE_RESULTS.NORMALIZED_VALUES, OUTPUTS_TENSOR, {
-        validationSplit: 0.15,
+        //validationSplit: 0.15,
+        callbacks: { onEpochEnd: logProgress },
         shuffle: true,
-        batchSize: 64,
-        epochs: 18
+        batchSize: 2,
+        epochs: 200
     });
 
     OUTPUTS_TENSOR.dispose();
     FEATURE_RESULTS.NORMALIZED_VALUES.dispose();
 
     console.log(`Average error loss: ${Math.sqrt(results.history.loss[results.history.loss.length - 1])}`);
-    console.log(`Average validation error loss: ${Math.sqrt(results.history.val_loss[results.history.val_loss.length - 1])}`);
+    //console.log(`Average validation error loss: ${Math.sqrt(results.history.val_loss[results.history.val_loss.length - 1])}`);
 
     function evaluate() {
         tf.tidy(function() {
-            let newInput = normalize(tf.tensor2d([[750, 1]]), FEATURE_RESULTS.MIN_VALUES, FEATURE_RESULTS.MAX_VALUES);
+            let newInput = normalize(tf.tensor1d([7]), FEATURE_RESULTS.MIN_VALUES, FEATURE_RESULTS.MAX_VALUES);
 
             let output = model.predict(newInput.NORMALIZED_VALUES);
             output.print();
